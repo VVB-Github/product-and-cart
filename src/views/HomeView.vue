6aed8db59560953b3ec4dbadf26149676a408149
VVB-Github <vbadaskin@gmail.com>
2024-02-10 23:23:27 +0300

    v3-pages_contents_added_up

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,76 @@
 <template>
   <div class="home">
     home
+    <div class="splash-container">
+      <div class="splash">
+        <h1>Splendid Food</h1>
+      </div>
+    </div>
+
+    <main class="wrapper">
+
+      <h2>Recommended</h2>
+
+      <div class="recommended">
+
+        <!-- проходимся по всему инвентарю и для каждого элемента создаём карточку
+        slice - даёт нам лишь 3 верхних элемента -->
+        <div v-for="(product,i) in inventory.slice(0,3)" :key="product.id" class="card">
+          <div class="card-title">
+            {{ product.name }}
+          </div>
+          <div class="card-body">
+            <i class="icofont-10x icofont-{{ product.icon }}"></i>
+            <form>
+              <div class="row">
+                <div class="cell">
+                  <label>Type:</label>
+                </div>
+                <div class="cell">
+                  <em>{{ product.type }}</em>
+                </div>
+              </div>
+              <div class="row">
+                <div class="cell">
+                  <label>Price:</label>
+                </div>
+                <div class="cell">
+                  ${{ product.price.USD }}
+                </div>
+              </div>
+              <div class="row">
+                <div class="cell">
+                  <label>Quantity:</label>
+                </div>
+                <div class="cell">
+                  <input type="number" v-model.number="product.quantity">
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="card-footer">
+            <button @click="addToCart(product.name, i)" class="btn btn-light">
+              Add to cart
+            </button>
+          </div>
+        </div>
+
+      </div>
+
+    </main>
+
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
+import food from '../food.json'
 
 export default {
   name: 'HomeView',
+  data () {
+    return {
+      inventory: food
+    }
+  },
   components: {
   }
 }
